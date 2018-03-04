dcdc1fd5ad52d3b0d488c7addf4563ac89ef57dd
Wojciech Waniek <wwaniek5@wp.pl>
2018-03-04 16:37:29 +0100

    Created solution

---
+++ Source/MouseHeatmap/MouseHeatmap/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseHeatmap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
