c1118b11326de62f6eacc41da5d60a2179fe019f
Wojciech Waniek <wwaniek5@wp.pl>
2018-03-11 18:45:56 +0100

    Working mause position reading

---
+++ Source/MouseHeatmap.Collector/Program.cs
@@ -0,0 +1,43 @@
+﻿using Gma.System.MouseKeyHook;
+using Serilog;
+using System;
+using Topshelf;
+
+namespace MouseHeatmap.Collector
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            HostFactory.Run(x =>
+            {
+                ConfigureLogger();
+
+                x.UseSerilog();
+
+                x.UseAssemblyInfoForServiceInfo();
+
+                x.Service(settings => new Service(), s =>
+                {
+                    s.BeforeStartingService(_ => Console.WriteLine("BeforeStart"));
+                    s.BeforeStoppingService(_ => Console.WriteLine("BeforeStop"));
+                });
+
+                x.OnException((exception) =>
+                {
+                    Console.WriteLine("Exception thrown - " + exception.Message);
+                });
+            });
+        }
+
+        private static void ConfigureLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .WriteTo.RollingFile("C:/Logs/MouseHeatmapLogs/MouseHeatmapCollector.log")
+                .CreateLogger();
+        }
+    }
+}
---
+++ Source/MouseHeatmap.Collector/Service.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+using Serilog;
+using Topshelf;
+
+namespace MouseHeatmap.Collector
+{
+    internal class Service: ServiceControl
+    {
+        private IKeyboardMouseEvents m_GlobalHook;
+
+        public bool Start(HostControl hostControl)
+        {
+            Log.Information("starting");
+
+
+            Do(Application.Exit);
+            Application.Run(new ApplicationContext());
+
+            return true;
+        }
+
+        public static void Do(Action quit)
+        {
+            Console.WriteLine("Press Q to quit.");
+            Hook.GlobalEvents().KeyPress += (sender, e) =>
+            {
+                Console.Write(e.KeyChar);
+                if (e.KeyChar == 'q') quit();
+            };
+
+
+            Hook.GlobalEvents().MouseMove += (sender, e) =>
+            {
+                Console.Write(e.X + " , " + e.Y);
+            };
+        }
+
+        private void MouseMoveHook(object sender, MouseEventArgs e)
+        {
+            Log.Debug(e.X+" , "+e.Y);
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            Log.Information("stopping");
+            return true;
+        }
+    }
+}
